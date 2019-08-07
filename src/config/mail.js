72e5f843de99c7e190587b9c4701bc6301b2e11b
Gustavo Fontana Margutti <gustavo.margutti@hotmail.com>
2019-08-06 22:03:50 -0300

    Config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'ce65f4767f3b8f',
+    pass: '1951635c507997',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe MeetApp <noreply@meetapp.com>',
+  },
+};
